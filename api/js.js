3e782e998f1e79af0257ec7ef3174d93fbf537ba
Imagambo <Ianmagambo@Gmail.com>
2026-03-26 14:13:37 -0400

    update proxy

--- api/js.js
+++ api/js.js
@@ -1,17 +1,20 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept,X-API-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept,X-API-Type,x-js-key-name,x-js-api-key');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { endpoint, ...params } = req.query;
   const url = new URL('https://developer.junglescout.com/api/' + (endpoint || ''));
   Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
 
+  const keyName = req.headers['x-js-key-name'] || '';
+  const apiKey = req.headers['x-js-api-key'] || '';
+
   const headers = {
     'Accept': 'application/vnd.junglescout.v1+json',
     'Content-Type': 'application/vnd.api+json',
-    'Authorization': req.headers['authorization'] || '',
+    'Authorization': `${keyName}:${apiKey}`,
     'X-API-Type': 'junglescout'
   };
 
