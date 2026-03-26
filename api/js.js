f5588dcef55c524ad285a5b604061de2a1c6f07b
Imagambo <Ianmagambo@Gmail.com>
2026-03-26 17:03:57 -0400

    Update js.js

--- api/js.js
+++ api/js.js
@@ -1,29 +1,31 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept,X-API-Type,x-js-key-name,x-js-api-key');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const { endpoint, ...params } = req.query;
-  const url = new URL('https://developer.junglescout.com/api/' + (endpoint || ''));
-  Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
+  try {
+    const { endpoint, ...params } = req.query;
+    const url = new URL('https://developer.junglescout.com/api/' + (endpoint || ''));
+    Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
 
-  const keyName = req.headers['x-js-key-name'] || '';
-  const apiKey = req.headers['x-js-api-key'] || '';
+    const auth = req.headers['authorization'] || 
+                 req.headers['x-auth'] || '';
 
-  const headers = {
-    'Accept': 'application/vnd.junglescout.v1+json',
-    'Content-Type': 'application/vnd.api+json',
-    'Authorization': `${keyName}:${apiKey}`,
-    'X-API-Type': 'junglescout'
-  };
+    const response = await fetch(url.toString(), {
+      method: req.method,
+      headers: {
+        'Accept': 'application/vnd.junglescout.v1+json',
+        'Content-Type': 'application/vnd.api+json',
+        'Authorization': auth,
+        'X-API-Type': 'junglescout'
+      },
+      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+    });
 
-  const upstream = await fetch(url.toString(), {
-    method: req.method,
-    headers,
-    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
-  });
-
-  const data = await upstream.json();
-  res.status(upstream.status).json(data);
+    const text = await response.text();
+    res.status(response.status).send(text);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
 }
